9bffaaca47ad6e4fcd8f2e51d99794be41c7a0c4
Duaa-14 <22308669@student.ciu.edu.tr>
2026-09-16 04:15:22 +0300

    Prepare project for deployment on Render (gunicorn, Procfile, PORT env var)

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir src app_flask:app --bind 0.0.0.0:$PORT
--- src/app_flask.py
+++ src/app_flask.py
@@ -1,5 +1,6 @@
 import io
 import json
+import os
 import uuid
 from pathlib import Path
 
@@ -270,4 +271,5 @@ def download_profile():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
